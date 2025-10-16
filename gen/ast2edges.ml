64db740bbe696b0abeadf74bcc4c9bc1b644831e
Filippo Sestini <sestini.filippo@gmail.com>
2025-10-16 12:13:23 +0100

    wip

--- gen/ast2edges.ml
+++ gen/ast2edges.ml
@@ -1,5 +1,7 @@
 [@@@warning "-40-42"]
 
+module StringMap = Map.Make (String)
+
 module NonEmpty = struct
   type 'a t = Cons of 'a * 'a list [@@deriving show]
 
@@ -76,8 +78,9 @@ let to_core_ast ~(unroll : int) ~(conds : string list) (exp : AST.exp) :
     | Op1 (_, ToId, exp) -> ToId (go exp)
     | Op1 (_, Plus, exp) -> unrolled (go exp)
     | Op1 (_, Star, exp) ->
-        CoreAST.union_of_list [ unrolled (go exp); Var "ignore" ]
-    | Op1 (_, Opt, exp) -> CoreAST.union_of_list [ go exp; Var "ignore" ]
+        CoreAST.union_of_list [ unrolled (go exp); Empty ]
+        (* CoreAST.union_of_list [ unrolled (go exp); Var "ignore" ] *)
+    | Op1 (_, Opt, exp) -> CoreAST.union_of_list [ go exp; Empty ]
     | Op1 (_, Inv, exp) -> Inv (go exp)
     | Op1 (_, Comp, exp) -> Comp (go exp)
     | Konst (_, Empty _) -> Empty
@@ -182,6 +185,8 @@ module IR = struct
   let pure (x : 'a) : ('a, 'b) t =
     pure_union (pure_seq (pure_rel_inter (pure_inter x)))
 
+  let pure_set (x : 'a) : 'a set = pure_union (pure_inter x)
+
   let union : 'a union -> 'a union -> 'a union =
    fun (Union x) (Union y) -> Union (List.append x y)
 
@@ -193,62 +198,100 @@ module IR = struct
   (*   | _ -> None *)
 end
 
-type builtin_rel =
+type builtin_fence =
   | ISB
   | DMB_SY
+  | DSB_LD
+  | DSB_SY
+  | DMB_LD
   | DMB_ISH
+  | DSB_ISH
+  | DMB_OSH
+  | DSB_OSH
   | DMB_OSHST
-  | DMB_ST
   | DMB_ISHST
+  | DMB_ISHLD
+  | DSB_ISHLD
+  | DMB_ST
   | DSB_ST
   | DSB_OSHST
   | DSB_ISHST
+  | DMB_OSHLD
+  | DSB_OSHLD
+
+type builtin_rel = Co | Fr | Rf | Ext | Po | Fencerel of builtin_fence
+
+let builtin_rels : (var * builtin_rel) list =
+  [ ("co", Co); ("fr", Fr); ("rf", Rf); ("ext", Ext); ("po", Po) ]
 
 type builtin_set =
+  | Exp
+  | M
   | R
   | W
-  | ISB
-  | DMB_SY
-  | DMB_ISH
-  | DMB_OSHST
-  | DMB_ISHST
-  | DMB_ST
-  | DSB_ST
-  | DSB_OSHST
-  | DSB_ISHST
+  | A
+  | L
+  | Q
+  | FAULT
+  | NoRet
+  | IC
+  | Fence of builtin_fence
 
 let fencerel : builtin_set -> builtin_rel = function
-  | ISB -> ISB
-  | DMB_ISH -> DMB_ISH
-  | DMB_SY -> DMB_SY
-  | DMB_OSHST -> DMB_OSHST
-  | DMB_ST -> DMB_ST
-  | DMB_ISHST -> DMB_ISHST
-  | DSB_ST -> DSB_ST
-  | DSB_ISHST -> DSB_ISHST
-  | DSB_OSHST -> DSB_OSHST
+  | Fence f -> Fencerel f
   | _ -> raise (NormalizationError "unsupported argument for fencerel")
 
-let parse_builtin_set : string -> builtin_set option = function
-  | "DMB.SY" -> Some DMB_SY
-  | "DMB.ISH" -> Some DMB_ISH
-  | "DMB.OSHST" -> Some DMB_OSHST
-  | "DMB.ST" -> Some DMB_ST
-  | "DMB.ISHST" -> Some DMB_ISHST
-  | "DSB.ST" -> Some DSB_ST
-  | "DSB.ISHST" -> Some DSB_ISHST
-  | "DSB.OSHST" -> Some DSB_OSHST
-  | "R" -> Some R
-  | "W" -> Some W
-  | _ -> None
+let builtin_sets : (var * builtin_set) list =
+  [
+    ("DSB.LD", Fence DSB_LD);
+    ("DMB.SY", Fence DMB_SY);
+    ("DSB.SY", Fence DSB_SY);
+    ("DMB.LD", Fence DMB_LD);
+    ("DMB.ISH", Fence DMB_ISH);
+    ("DMB.OSHST", Fence DMB_OSHST);
+    ("DMB.ST", Fence DMB_ST);
+    ("DMB.ISHST", Fence DMB_ISHST);
+    ("DSB.ST", Fence DSB_ST);
+    ("DSB.ISHST", Fence DSB_ISHST);
+    ("DSB.OSHST", Fence DSB_OSHST);
+    ("DMB.ISHLD", Fence DMB_ISHLD);
+    ("DSB.ISH", Fence DSB_ISH);
+    ("DSB.ISHLD", Fence DSB_ISHLD);
+    ("DMB.OSH", Fence DMB_OSH);
+    ("DSB.OSH", Fence DSB_OSH);
+    ("DMB.OSHLD", Fence DMB_OSHLD);
+    ("DSB.OSHLD", Fence DSB_OSHLD);
+    ("ISB", Fence ISB);
+    ("R", R);
+    ("W", W);
+    ("M", M);
+    ("A", A);
+    ("L", L);
+    ("Q", Q);
+    ("Exp", Exp);
+    ("FAULT", FAULT);
+    ("NoRet", NoRet);
+    ("IC", IC);
+  ]
 
 let comp_set_inter : builtin_set IR.inter -> builtin_set IR.set =
  (* TODO: implement as in cat2config *)
  fun is ->
   IR.pure_union is
 
-let to_set ~(env : (var * builtin_set IR.set) list) (t : var CoreAST.t) :
-    builtin_set IR.set =
+type ir_set = builtin_set IR.set
+type ir = (builtin_rel, builtin_set) IR.t
+type env = (ir, ir_set) Either.t StringMap.t
+
+let comp_rel_inter : builtin_rel IR.inter -> ir =
+ (* TODO: implement as in cat2config *)
+ fun is ->
+  IR.(pure_union (pure_seq (pure_rel_inter is)))
+
+let domain : ir -> ir_set = _
+let range : ir -> ir_set = _
+
+let rec to_set ~(env : env) (t : var CoreAST.t) : ir_set =
   let rec go : var CoreAST.t -> builtin_set IR.set = function
     | Union es -> es |> NonEmpty.map go |> NonEmpty.foldl1 IR.union
     | Inter es -> es |> NonEmpty.map go |> NonEmpty.foldl1 IR.inter_set
@@ -258,34 +301,35 @@ let to_set ~(env : (var * builtin_set IR.set) list) (t : var CoreAST.t) :
         let complemented = List.map comp_set_inter aux in
         List.fold_left IR.inter_set (Union []) complemented
     | Var v -> (
-        match parse_builtin_set v with
-        | Some s -> IR.pure_union (IR.pure_inter s)
-        | None -> (
-            match List.assoc_opt v env with
-            | Some t -> t
-            | None ->
-                let err = Format.sprintf "unknown set variable: %s" v in
-                raise (NormalizationError err)))
-    | _ -> raise (NormalizationError "set expression not supported")
+        match StringMap.find_opt v env with
+        | Some (Right t) -> t
+        | _ ->
+            let err = Format.sprintf "unknown set variable: %s" v in
+            raise (NormalizationError err))
+    | App (fn, e) -> (
+        match fn with
+        | Domain -> domain (to_ir ~env e)
+        | Range -> range (to_ir ~env e)
+        | Fencerel ->
+            let err = "Function `fencerel` does not produce a set" in
+            raise (NormalizationError err))
+    | e ->
+        let err =
+          Format.sprintf "invalid or unsupported set expression: %s"
+            (CoreAST.show Format.pp_print_string e)
+        in
+        raise (NormalizationError err)
   in
   go t
 
-type ir = (builtin_rel, builtin_set) IR.t
-
-let comp_rel_inter : builtin_rel IR.inter -> ir =
- (* TODO: implement as in cat2config *)
- fun is ->
-  IR.(pure_union (pure_seq (pure_rel_inter is)))
-
 (* TODO: account for recursive definitions *)
-let to_ir ~(env : (var * ir) list) ~(set_env : (var * builtin_set IR.set) list)
-    (t : var CoreAST.t) : ir =
+and to_ir ~(env : env) (t : var CoreAST.t) : ir =
   let rec go : var CoreAST.t -> ir = function
     | Union es -> es |> NonEmpty.map go |> NonEmpty.foldl1 IR.union
     | Seq es -> es |> NonEmpty.map go |> NonEmpty.foldl1 IR.seq
     | Inter es -> es |> NonEmpty.map go |> NonEmpty.foldl1 IR.inter_t
     | ToId e ->
-        let aux = to_set ~env:set_env e in
+        let aux = to_set ~env e in
         IR.map_union (fun is -> IR.(pure_seq (Set is))) aux
     | Inv e -> go e |> IR.map_union IR.rev_seq
     | Comp e ->
@@ -301,31 +345,73 @@ let to_ir ~(env : (var * ir) list) ~(set_env : (var * builtin_set IR.set) list)
     | App (fn, e) -> (
         match fn with
         | Fencerel -> (
-            let s = to_set ~env:set_env e in
+            let s = to_set ~env e in
             match IR.as_pure_set s with
             | Some x ->
                 let r = fencerel x in
                 IR.pure r
             | None -> raise (NormalizationError "fencerel failed"))
-        | _ ->
-            raise (NormalizationError "unsupported function")
-            (* app_ir fn (go e) *))
+        | Domain ->
+            let err = "Function `domain` does not produce a relation" in
+            raise (NormalizationError err)
+        | Range ->
+            let err = "Function `range` does not produce a relation" in
+            raise (NormalizationError err))
     | Empty -> Union []
     | Var v -> (
-        match List.assoc_opt v env with
-        | Some t -> t
-        | None ->
+        match StringMap.find_opt v env with
+        | Some (Left t) -> t
+        | _ ->
             let err = Format.sprintf "unsupported rel var: %s" v in
             raise (NormalizationError err))
   in
   go t
+
+module DList = struct
+  type 'a t = DList of ('a list -> 'a list)
+
+  let of_list xs = DList (fun ys -> List.append xs ys)
+  let empty = of_list []
+  let to_list (DList l) = l []
+  let append (DList l1) (DList l2) = DList (fun xs -> l1 (l2 xs))
+end
+
+let extract_let_bindings (ins : AST.ins list) : (var * AST.exp) list =
+  let aux =
+    ins
+    |> List.fold_left
+         (fun l e ->
+           let open AST in
+           let l' =
+             match e with
+             | Let (_, xs) ->
+                 xs
+                 |> List.filter_map (function
+                      | _, Pvar (Some v), expr -> Some (v, expr)
+                      | _ -> None)
+                 |> DList.of_list
+             | Rec (_, (_, Pvar (Some v), expr) :: rest, _) ->
+                 if List.length rest > 0 then
+                   Format.eprintf "Skipping %d Rec bindings@."
+                     (List.length rest);
+                 DList.of_list [ (v, expr) ]
+             | _ ->
+                 (* Format.eprintf "Skipping instruction@."; *)
+                 DList.empty
+           in
+           DList.append l l')
+         DList.empty
+  in
+  DList.to_list aux
 
 let parse_core ~(unroll : int) ~(conds : string list)
     (lets : (var * AST.exp) list) : (var * var CoreAST.t) list =
   lets
   |> List.filter_map (fun (v, e) ->
          try
+           (* let () = Format.eprintf "Parsing core for %s@." v in *)
            let core = to_core_ast ~unroll ~conds e in
+           (* let () = Format.eprintf "Setting core for %s@." v in *)
            Some (v, core)
          with NormalizationError _err ->
            (* Format.eprintf "parse_core (%s): %s@." v err; *)
@@ -333,21 +419,39 @@ let parse_core ~(unroll : int) ~(conds : string list)
 
 let normalize_core (core_bindings : (var * var CoreAST.t) list) :
     (var * ir) list =
-  let rel_lets, _ =
+  let env : env = StringMap.empty in
+  let env =
+    builtin_sets
+    |> List.fold_left
+         (fun env (v, t) -> StringMap.add v (Either.Right (IR.pure_set t)) env)
+         env
+  in
+  let env =
+    builtin_rels
+    |> List.fold_left
+         (fun env (v, t) -> StringMap.add v (Either.Left (IR.pure t)) env)
+         env
+  in
+  let env =
     core_bindings
     |> List.fold_left
-         (fun (env, set_env) (v, t) ->
+         (fun env (v, t) ->
            try
-             let s = to_set ~env:set_env t in
-             (env, (v, s) :: set_env)
+             let s = to_set ~env t in
+             (* let () = Format.eprintf "setting %s (set)@." v in *)
+             let env = StringMap.add v (Either.Right s) env in
+             env
            with NormalizationError err -> (
-             Format.eprintf "normalize_core(set)(%s): %s@." v err;
+             (* Format.eprintf "normalize_core(set)(%s): %s@." v err; *)
              try
-               let t = to_ir ~env ~set_env t in
-               ((v, t) :: env, set_env)
+               let t = to_ir ~env t in
+               (* let () = Format.eprintf "setting %s (rel)@." v in *)
+               let env = StringMap.add v (Either.Left t) env in
+               env
              with NormalizationError err ->
                Format.eprintf "normalize_core(rel)(%s): %s@." v err;
-               (env, set_env)))
-         ([], [])
+               env))
+         env
   in
-  rel_lets
+  env |> StringMap.to_list
+  |> List.filter_map (function v, Either.Left t -> Some (v, t) | _ -> None)
