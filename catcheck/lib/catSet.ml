3c6d21d9ed95b73790d1e54ee5dd5188661418fa
Filippo Sestini <sestini.filippo@gmail.com>
2026-02-22 17:48:17 +0000

    wip: represent sets as union of partitions

--- catcheck/lib/catSet.ml
+++ catcheck/lib/catSet.ml
@@ -1,42 +1,12 @@
-module Atom = struct
-  type t = Pure_R | A | Pure_W | L | MMU_fault | Other_fault | Unknown
+type t = IntSet.t
 
-  let compare = Stdlib.compare
-
-  let to_string = function
-    | Pure_R -> "R\\A"
-    | Pure_W -> "W\\L"
-    | A -> "A"
-    | L -> "L"
-    | MMU_fault -> "MMU"
-    | Other_fault -> "Fault\\MMU"
-    | Unknown -> "U\\(...)"
-end
-
-module S = Set.Make (Atom)
-
-type t = S.t
-
-let universe : t =
-  S.of_list Atom.[ Pure_R; Pure_W; A; L; MMU_fault; Other_fault; Unknown ]
-
-(* let rec of_ident = function *)
-let rec of_primitive_set = function
-  | "R" -> S.of_list Atom.[ Pure_R; A ]
-  | "W" -> S.of_list Atom.[ Pure_W; L ]
-  | "A" -> S.of_list Atom.[ A ]
-  | "L" -> S.of_list Atom.[ L ]
-  | "M" -> S.union (of_primitive_set "R") (of_primitive_set "W")
-  | "MMU" -> S.singleton Atom.MMU_fault
-  | "Fault" -> S.of_list Atom.[ MMU_fault; Other_fault ]
-  | "_" -> universe
-  | s -> invalid_arg (Format.sprintf "unknown set identifier: %s" s)
-
-let empty : t = S.empty
-let union (s1 : t) (s2 : t) : t = S.union s1 s2
-let inter (s1 : t) (s2 : t) : t = S.inter s1 s2
-let is_subset s1 s2 = S.subset s1 s2
-let equal s1 s2 = S.equal s1 s2
+let universe : t = Partitions.of_set_name "_"
+let of_primitive_set = Partitions.of_set_name
+let empty : t = IntSet.empty
+let union (s1 : t) (s2 : t) : t = IntSet.union s1 s2
+let inter (s1 : t) (s2 : t) : t = IntSet.inter s1 s2
+let is_subset s1 s2 = IntSet.subset s1 s2
+let equal s1 s2 = IntSet.equal s1 s2
 
 let rec of_set_expr (e : AST.exp) : t =
   let open AST in
@@ -44,37 +14,67 @@ let rec of_set_expr (e : AST.exp) : t =
   | Var (_, s) -> of_primitive_set s
   | Op (_, Union, es) -> List.fold_left union empty (List.map of_set_expr es)
   | Op (_, Inter, es) -> List.fold_left inter universe (List.map of_set_expr es)
-  | Op (_, Diff, [ x; y ]) -> S.diff (of_set_expr x) (of_set_expr y)
-  | Op1 (_, Inv, e) -> S.diff universe (of_set_expr e)
+  | Op (_, Diff, [ x; y ]) -> IntSet.diff (of_set_expr x) (of_set_expr y)
+  | Op1 (_, Inv, e) -> IntSet.diff universe (of_set_expr e)
   | _ -> failwith "invalid set exp"
 
-(* module StringSet = Set.Make (String) *)
-(**)
-(* let to_string_set : t -> StringSet.t = *)
-(*  fun t -> StringSet.of_list (List.map Atom.to_string (S.to_list t)) *)
-(**)
-(* let to_pretty_string_set t = *)
-(*   if S.equal t universe then StringSet.singleton "Univ" *)
-(*   else *)
-(*     let str_s = to_string_set t in *)
-(*     let r_set = of_ident "R" |> to_string_set in *)
-(*     let str_s = *)
-(*       if StringSet.subset r_set str_s then *)
-(*         StringSet.add "R" (StringSet.diff str_s r_set) *)
-(*       else str_s *)
-(*     in *)
-(*     let w_set = of_ident "W" |> to_string_set in *)
-(*     let str_s = *)
-(*       if StringSet.subset w_set str_s then *)
-(*         StringSet.add "W" (StringSet.diff str_s w_set) *)
-(*       else str_s *)
-(*     in *)
-(*     str_s *)
+let simplify_intersection : bool StringMap.t -> bool StringMap.t =
+  let should_include sol s b =
+    if b then
+      let subs = Partitions.subsets s in
+      StringSet.for_all (fun sub -> not (StringMap.find sub sol)) subs
+    else
+      let sups =
+        StringSet.diff (Partitions.supersets s) (StringSet.singleton "_")
+      in
+      let disjs = Partitions.disjoints s in
+      StringSet.for_all (fun sup -> StringMap.find sup sol) sups
+      && not (StringSet.exists (fun disj -> StringMap.find disj sol) disjs)
+  in
+  fun m -> StringMap.filter (should_include m) m
+
+let join_intersection : bool StringMap.t -> bool StringMap.t -> bool StringMap.t
+    =
+ fun m1 m2 ->
+  StringMap.fold
+    (fun k v acc ->
+      let v' =
+        try StringMap.find k m2
+        with Not_found ->
+          failwith (Format.sprintf "Not_found in join_intersection: %s" k)
+      in
+      StringMap.add k (v || v') acc)
+    m1 StringMap.empty
+
+let pp_intersection fmt sol =
+  let open Format in
+  sol |> StringMap.to_list
+  |> pp_print_list
+       ~pp_sep:(fun fmt () -> pp_print_string fmt " & ")
+       (fun fmt (l, b) ->
+         let pfx = if b then "" else "~" in
+         fprintf fmt "%s%s" pfx l)
+       fmt
+
+let to_intersection (t : t) : bool StringMap.t option =
+  match IntSet.to_list t with
+  | [] -> None
+  | x :: xs ->
+      List.fold_left
+        (fun acc i -> join_intersection (Partitions.bitmap_of_partition i) acc)
+        (Partitions.bitmap_of_partition x)
+        xs
+      |> simplify_intersection |> Option.some
 
 let pp fmt t =
-  let l = t |> S.to_list |> List.map Atom.to_string in
-  Format.fprintf fmt "%s" (String.concat " ⊔ " l)
-
-(* let pp fmt t = *)
-(*   let s = to_pretty_string_set t in *)
-(*   Format.fprintf fmt "%s" (String.concat " ⊔ " (StringSet.to_list s)) *)
+  match
+    List.find_opt
+      (fun nm -> IntSet.equal t (Partitions.of_set_name nm))
+      Partitions.set_names
+  with
+  | Some nm -> Format.pp_print_string fmt nm
+  | None -> (
+      match to_intersection t with
+      | Some inter -> Format.fprintf fmt "%a" pp_intersection inter
+      | None -> Format.fprintf fmt "emptyset")
+(* (String.concat " ⊔ " (StringSet.to_list s)) *)
