0610a2b88ee64db1377dc1448a74175619c648e6
Filippo Sestini <filippo.sestini@arm.com>
2026-02-27 18:46:10 +0000

    chore: remove comments

--- catcheck/lib/catSet.ml
+++ catcheck/lib/catSet.ml
@@ -33,19 +33,6 @@ let simplify_intersection : bool StringMap.t -> bool StringMap.t =
   in
   fun m -> StringMap.filter (should_include m) m
 
-(* let join_intersection : bool StringMap.t -> bool StringMap.t -> bool StringMap.t *)
-(*     = *)
-(*  fun m1 m2 -> *)
-(*   StringMap.fold *)
-(*     (fun k v acc -> *)
-(*       let v' = *)
-(*         try StringMap.find k m2 *)
-(*         with Not_found -> *)
-(*           failwith (Format.sprintf "Not_found in join_intersection: %s" k) *)
-(*       in *)
-(*       StringMap.add k (v || v') acc) *)
-(*     m1 StringMap.empty *)
-
 let pp_intersection fmt sol =
   let open Format in
   sol |> StringMap.to_list
@@ -56,22 +43,9 @@ let pp_intersection fmt sol =
          fprintf fmt "%s%s" pfx l)
        fmt
 
-(* let to_intersection (t : t) : bool StringMap.t option = *)
-(*   match IntSet.to_list t with *)
-(*   | [] -> None *)
-(*   | x :: xs -> *)
-(*       List.fold_left *)
-(*         (fun acc i -> join_intersection (Partitions.bitmap_of_partition i) acc) *)
-(*         (Partitions.bitmap_of_partition x) *)
-(*         xs *)
-(*       |> simplify_intersection |> Option.some *)
-
 let predefined_sets =
   let of_prim x = of_primitive_set x |> Option.get in
   [
-    (* ("M", of_prim "M"); *)
-    (* ("R", of_prim "R"); *)
-    (* ("W", of_prim "W"); *)
     ("Exp & R", inter (of_prim "Exp") (of_prim "R"));
     ("Exp & W", inter (of_prim "Exp") (of_prim "W"));
     ("Exp & M", inter (of_prim "Exp") (of_prim "M"));
@@ -103,12 +77,3 @@ let pp =
       in
       let str = String.concat " | " (disjs @ rest_disjs) in
       Format.pp_print_string fmt str
-
-(* match List.find_opt (fun (_nm, t') -> IntSet.equal t t') predefined_sets with *)
-(* | Some (nm, _) -> Format.pp_print_string fmt nm *)
-(* | None -> ( *)
-(*     match to_intersection t with *)
-(*     | Some inter -> Format.fprintf fmt "%a" pp_intersection inter *)
-(*     | None -> Format.fprintf fmt "emptyset") *)
-
-(* (String.concat " ⊔ " (StringSet.to_list s)) *)
