b89d851b3f7f4b233eef978cf191f8691f070d34
Filippo Sestini <filippo.sestini@arm.com>
2026-05-17 11:09:32 +0100

    wip tests

---
+++ catcheck/test/test_fw_bw_analysis.ml
@@ -0,0 +1,34 @@
+open Catcheck
+
+module P = Cat.MakeParser (struct
+  (* let libdir = "test-libdir" *)
+  let libdir = "libdir"
+end)
+
+module E = TxtLoc.Extract ()
+
+let () =
+  (* let bs = P.read_bindings "test.cat" in *)
+  let bs = P.read_bindings "aarch64.cat" in
+  Top.run bs
+(* let module D = AbstractDomain.FromTyped (DRDomain) in *)
+(* let module G = Analysis.Make (D) in *)
+(* let results = G.solve_all bs in *)
+(* results *)
+(* |> List.iter (fun (loc, res) -> *)
+(*     let _combined = D.meet res.G.forward res.G.backward in *)
+(*     Printf.printf "%a:\n" TxtLoc.pp loc; *)
+(*     Printf.printf "  %s\n" (E.extract loc); *)
+(*     ()) *)
+(* if D.equal combined res.G.forward then *)
+(*   Printf.printf "%a: %s: OK.\n" TxtLoc.pp loc (E.extract loc) *)
+(* else *)
+(*   match combined with *)
+(*   | D.Rel r -> *)
+(*       let expected = CatSet.inter r.DRDomain.domain r.DRDomain.range in *)
+(*       Printf.printf "%a:\n" TxtLoc.pp loc; *)
+(*       Format.printf "  expression `%s` could be simplified to `[%a]`@." *)
+(*         (E.extract loc) CatSet.pp expected *)
+(*   | D.Set _ -> Format.printf "Found Set@." *)
+(*   | D.Top -> Format.printf "Found Top@." *)
+(*   | D.Bottom -> Format.printf "Found Bottom@.") *)
