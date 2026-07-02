f1bb2984e5b4fe600e12ee4491381f9cb6d6460c
Filippo Sestini <filippo.sestini@arm.com>
2026-07-02 11:15:37 +0100

    wip: LitmusTest

--- tools/loop-detection/lib/litmusTest.ml
+++ tools/loop-detection/lib/litmusTest.ml
@@ -28,47 +28,55 @@ let static_poi_of_label (lbl : Label.t) =
     let p = List.assoc proc (prog test) in
     go 0 p
 
-module MakeArch (C : sig
-  val is_morello : bool
-end) =
-struct
-  module SP = Splitter.Make (struct
-    let debug = false
-    let check_rename = fun s -> Some s
+let prog_section ~proc ~start_spoi ~end_spoi ltest =
+  let prog_ins = prog_instructions ltest in
+  let proc_prog = List.assoc proc prog_ins in
+  List.take (end_spoi - start_spoi + 1) (List.drop start_spoi proc_prog)
+
+module SP = Splitter.Make (struct
+  let debug = false
+  let check_rename = fun s -> Some s
+end)
+
+module C = struct
+  let is_morello = false
+end
+
+module AArch64Value = CapabilityValue.Make (C)
+module AArch64 = MakeAArch64Base.Make (C)
+
+module LexConfig = struct
+  let debug = false
+end
+
+module AArch64LexParse = struct
+  type instruction = AArch64.parsedPseudo
+  type token = AArch64Parser.token
+
+  module Lexer = AArch64Lexer.Make (struct
+    include LexConfig
+
+    let is_morello = C.is_morello
   end)
 
-  module AArch64Value = CapabilityValue.Make (C)
-  module AArch64 = MakeAArch64Base.Make (C)
-
-  module LexConfig = struct
-    let debug = false
-  end
-
-  module AArch64LexParse = struct
-    type instruction = AArch64.parsedPseudo
-    type token = AArch64Parser.token
-
-    module Lexer = AArch64Lexer.Make (struct
-      include LexConfig
-
-      let is_morello = C.is_morello
-    end)
-
-    let lexer = Lexer.token
-    let parser = AArch64Parser.main
-  end
-
-  module P0 =
-    GenParser.Make (GenParser.DefaultConfig) (AArch64) (AArch64LexParse)
-
-  type instruction = AArch64Base.instruction
-
-  let parse chan =
-    let (splitted : Splitter.result) = SP.split "T" chan in
-    P0.parse chan splitted
-
-  let parse_from_file = Misc.input_protect (fun ch -> parse ch)
-
-  let show_instruction (ins : AArch64Base.instruction) =
-    AArch64.dump_instruction ins
+  let lexer = Lexer.token
+  let parser = AArch64Parser.main
 end
+
+module P0 = GenParser.Make (GenParser.DefaultConfig) (AArch64) (AArch64LexParse)
+
+type instruction = AArch64Base.instruction
+
+let parse chan =
+  let (splitted : Splitter.result) = SP.split "T" chan in
+  P0.parse chan splitted
+
+let parse_from_file = Misc.input_protect (fun ch -> parse ch)
+
+let show_instruction (ins : AArch64Base.instruction) =
+  AArch64.dump_instruction ins
+
+let pp_prog_section fmt l =
+  let open Format in
+  let l = List.map show_instruction l in
+  pp_print_list ~pp_sep:(fun fmt () -> fprintf fmt "@,") pp_print_string fmt l
--- tools/loop-detection/lib/litmusTest.mli
+++ tools/loop-detection/lib/litmusTest.mli
@@ -5,11 +5,16 @@ val prog : test -> (proc * AArch64Base.pseudo list) list
 val prog_instructions : test -> (proc * AArch64Base.instruction list) list
 val static_poi_of_label : Label.t -> test -> proc -> int
 
-module MakeArch (C : sig
-  val is_morello : bool
-end) : sig
-  type instruction = AArch64Base.instruction
+val prog_section :
+  proc:int ->
+  start_spoi:int ->
+  end_spoi:int ->
+  test ->
+  AArch64Base.instruction list
 
-  val parse_from_file : string -> test
-  val show_instruction : instruction -> string
-end
+val pp_prog_section : Format.formatter -> AArch64Base.instruction list -> unit
+
+type instruction = AArch64Base.instruction
+
+val parse_from_file : string -> test
+val show_instruction : instruction -> string
